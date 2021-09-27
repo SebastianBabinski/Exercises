37eaa1931ea469508934c57db4c4624d2dfe8911
SebastianBabinski <babinski.sebastian@gmail.com>
2021-09-27 05:03:39 +0200

    7

---
+++ src/Cwiczenie7.java
@@ -0,0 +1,25 @@
+// check if number is prime number
+
+public class Cwiczenie7 {
+
+    public static void main(String[] args) {
+        isPrime(1);
+        isPrime(3);
+        isPrime(17);
+        isPrime(20);
+    }
+
+    static void isPrime(int number) {
+        int counter = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                counter++;
+            }
+        }
+        if (counter == 2) {
+            System.out.println(number + " is prime number");
+        } else {
+            System.out.println(number + " is not prime number");
+        }
+    }
+}
