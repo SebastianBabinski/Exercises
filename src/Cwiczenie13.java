5ed27709f8c86307b6f933edfef74ca3f22479e6
SebastianBabinski <babinski.sebastian@gmail.com>
2021-10-04 18:06:28 +0200

    12

---
+++ src/Cwiczenie13.java
@@ -0,0 +1,32 @@
+// reversing number
+
+import java.util.Scanner;
+
+public class Cwiczenie13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Welcome, input the number you want to reverse.");
+        while(!scanner.hasNextInt()) {
+            scanner.next();
+            System.err.println("Please enter a number.");
+        }
+        number = scanner.nextInt();
+        reverse(number);
+    }
+
+    static void reverse(int number) {
+
+        int reversedNumber = 0;
+        int remainder = 0;
+
+        do {
+            remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number = number / 10;
+        } while (number > 0);
+
+        System.out.println("Your reversed number is: " + reversedNumber);
+    }
+}
