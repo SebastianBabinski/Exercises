80c1b0b97d7f69c5d001e7dd454a6ca02e7cf1b7
SebastianBabinski <babinski.sebastian@gmail.com>
2021-10-24 23:05:41 +0200

    28

--- src/NWD.java
+++ src/NWD.java
@@ -1,12 +1,15 @@
 public class NWD {
 
     public static void main(String[] args) {
-
         System.out.println(NWDTest(15, 3));
 
     }
 
     public static int NWDTest(int a, int b) {
+        if(a <= 0 || b <= 0 ){
+            System.out.println("A and B should be higher than 0.");
+            System.exit(1);
+        }
         while (b != 0) {
             int c = a % b;
             a = b;
