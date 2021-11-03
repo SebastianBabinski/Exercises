1c8a565224c5bd3482997809227aa0aa7483c8ff
SebastianBabinski <babinski.sebastian@gmail.com>
2021-11-03 21:02:25 +0100

    29

--- src/NWD.java
+++ src/NWD.java
@@ -2,13 +2,15 @@ public class NWD {
 
     public static void main(String[] args) {
         System.out.println(NWDTest(15, 3));
+        System.out.println(NWDTest(0, 3));
+        System.out.println(NWDTest(6, 3));
 
     }
 
     public static int NWDTest(int a, int b) {
         if(a <= 0 || b <= 0 ){
             System.out.println("A and B should be higher than 0.");
-            System.exit(1);
+            return 0;
         }
         while (b != 0) {
             int c = a % b;
---
+++ src/findDuplicatesChallenge.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
+public class findDuplicatesChallenge {
+
+//    public static void main(String[] args) {
+//        System.out.println(findDuplicates(asList(-1, 1, 3, 2, 2, 2, 5, 6, -1, 3, 6), 2));
+//    }
+
+    public static List<Integer> findDuplicates(List<Integer> integers, int numberOfDuplicates) {
+        List<Integer> duplicates = new LinkedList<>();
+        int counter = 1;
+        int temp = 0;
+
+        for (int i = 0; i < integers.size(); i++) {
+            for (int j = 1; j < integers.size(); j++) {
+                if (integers.get(i).equals(integers.get(j))) {
+                    counter++;
+                }
+            }
+            if (counter == numberOfDuplicates) {
+                temp = integers.get(i);
+                duplicates.add(temp);
+            }
+            counter = 0;
+        }
+        Set<Integer> numbersWithoutDuplicates = new LinkedHashSet<Integer>(duplicates);
+        duplicates.clear();
+        duplicates.addAll(numbersWithoutDuplicates);
+        return duplicates;
+    }
+
+}
